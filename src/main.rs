03676146f8b112306ae8fc8b8b7377be9352226f
Aminu 'Seun Joshua <34725212+seun-ja@users.noreply.github.com>
2024-12-02 21:11:48 +0100

    first and second day

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use advent_of_code_24::days;
+
+fn main() {
+    // Day One
+    // days::one::solution_part_one();
+    // days::one::sulution_part_two();
+
+    // Day Two
+    // days::two::solution_part_one();
+    days::two::solution_part_two();
+}
